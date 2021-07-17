44cc5a61555da58781b74b1daa5d7c743b40b9ea
Matthew McKney <matthewmckney@Matthews-MacBook-Pro.local>
2021-07-17 16:08:53 -0400

    sat-mongo-api

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express') // import express
+const cors = require('cors') // import cors
+const mongoose = require('mongoose') // import mongoose
+require('dotenv/config')
+
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+mongoose
+    .connect(process.env.DB_CONNECTION,{
+        useNewUrlParser: true, 
+        useUnifiedTopology: true, 
+        useFindAndModify: false
+    })
+    .then(() => app.listen(5000, (req, res) => {
+        console.log('Houston were connected')
+    }))
+    .catch(err => console.log(err))
+
+const countryRoute = require('./src/routes/countryRoute')
+app.use(countryRoute)
+
